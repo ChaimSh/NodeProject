f1e3193c39c4c8a0a44388498742196088328216
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-09-08 21:33:37 -0500

    parsed data using middleware

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 
-app.use((req, res, next) => {
-   let body = '';
-   req.on('end', () => {
-       const userNme = body.split('=')[1];
-       if (userName) {
-       req.body = { name: userName };
-       }
-       next();
-   })
-    req.on('data', chunk => {
-      body += chunk
-   })
-});
+app.use(bodyParser.urlencoded({ extended: false}));
 
+app.post('/user', (req, res, next) => {
+   res.send('<h1>' + req.body.username + '</h1>');
+})
 //call next if you dont wanna call this middle
-app.use((req, res, next) => {
-    if (req.body) {
-        return res.send('<h1>' + req.body.name + '</h1>');
-    }
-    res.send('<form method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>')
+app.get('/', (req, res, next) => {
+    res.send('<form action="user" method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>')
 });
 
 app.listen(5000);
